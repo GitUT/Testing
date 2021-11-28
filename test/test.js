83a7213e9d19d489c40e196a05a5935798c210a6
Markus Uusitalo <markus.uusitalo@hotmail.fi>
2021-11-28 08:12:03 +0200

    coveralls

--- test/test.js
+++ test/test.js
@@ -1,9 +1,12 @@
 const assert = require('chai').assert;
 
+const kek = require('../index');
 
-describe('Array', function () {
+
+describe('Juuh', function () {
     describe('Hello World', function() {
         it("should pass", function() {
+            kek();
             assert.typeOf("ggg", "string");
         })
     })
