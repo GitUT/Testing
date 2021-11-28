92756e8f013c6e38a9d5d577234d8bad1f7d0999
Markus Uusitalo <markus.uusitalo@hotmail.fi>
2021-11-28 06:39:21 +0200

    added a mock test

---
+++ test/test.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert;
+
+
+describe('Array', function () {
+    describe('Hello World', function() {
+        it("should pass", function() {
+            assert.typeOf("ggg", "string");
+        })
+    })
+})
