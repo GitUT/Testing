0a18f7b2c660b175cd1df7073a2dd599ecd9045b
Markus Uusitalo <markus.uusitalo@hotmail.fi>
2021-11-30 02:36:13 +0200

    added a testcase to add.js tests

--- test/add.test.js
+++ test/add.test.js
@@ -3,8 +3,11 @@ import add from "../src/add.js";
 
 // Testing add function with different inputs
 describe("add", () => {
+    // Testcase ID
     let i = 1;
 
+    // Creates a test case with a description, adds two numbers using the add function
+    // and compares the result to the expected result
     function testCase(description, a, b, result) {
         it(`${i}. ${description}`, () => {
             expect(add(a, b)).to.eql(result);
@@ -17,4 +20,5 @@ describe("add", () => {
     testCase("adds a positive decimal number and a negative decimal number together", 5454.5, -99.99, 5354.51);
     testCase("adds two negative decimal numbers together", -24.5, -59.9, -84.4);
     testCase("adds a negative decimal number and 0 together", -20.25, 0, -20.25);
+    testCase("adds 0 and 0 together", 0, 0, 0);
 });
